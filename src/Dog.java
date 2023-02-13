f46dcc328ab2c32ec602f2a95755496d0838acc0
alberth2025 <alberth2025@student.cis.edu.hk>
2023-02-13 12:37:33 +0800

    commit

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,58 @@
 public class Dog {
+    //properties
+    private int age;
+    private String name;
+    private String color;
+
+    //constructor
+    public Dog(){
+        age = 0;
+        name = "";
+        color = "";
+    }
+
+    public Dog(int a, String n, String c){
+        age = a;
+        name = n;
+        color = c;
+    }
+
+    //behavior
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private void addOneAge(){
+        age++;
+    }
+
+    public int birthday(){
+        addOneAge();
+        return age;
+    }
+
+    public String toString(){
+        return ("The dog is named " + name + " and is " + age + " years old. This dog is " + color + ".");
+    }
 }
